2ebf1e5769e88f6109af8600c08ee3b0ee3b4b0a
Bhargavkulla <118447823+Bhargavkulla@users.noreply.github.com>
2025-04-07 14:24:05 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,20 +1,22 @@
 import java.io.IOException;
-import java.net.InetSocketAddress;
+import java.io.OutputStream;
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.net.InetSocketAddress;
 
 public class HelloWorld {
     public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(5000), 0);
-        server.createContext("/", exchange -> {
-            String response = "Hello, World!";
-            exchange.sendResponseHeaders(200, response.length());
-            exchange.getResponseBody().write(response.getBytes());
-            exchange.getResponseBody().close();
-        });
+        // Create an HTTP server on port 8080
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new HelloHandler());
+        server.setExecutor(null); // Default executor
         server.start();
-        System.out.println("Server started at http://localhost:5000");
-
-        // Keep the program running
+        
+        // Keeping the server running indefinitely
+        System.out.println("Server started on http://localhost:8080");
+        
+        // This will make the main thread keep running
         while (true) {
             try {
                 Thread.sleep(1000);
@@ -23,5 +25,15 @@ public class HelloWorld {
             }
         }
     }
-}
 
+    static class HelloHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hello, World from Dockerized Java Application!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
